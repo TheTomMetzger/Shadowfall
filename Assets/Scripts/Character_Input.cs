133e50ab6b03e4dabf18471b5f1edc0e48333880
Tom Metzger <tom.metzger.ii@gmail.com>
2017-04-22 14:35:42 -0400

    Level 1 and Game Mechanics Complete

--- Assets/Scripts/Character_Input.cs
+++ Assets/Scripts/Character_Input.cs
@@ -1,31 +1,121 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Character_Input : MonoBehaviour {
+
+
+
+
+public class Character_Input : MonoBehaviour 
+{
 
     public float speed;
     private Rigidbody body;
 
+    public bool lightsOn;
+
+    public Camera camera;
+
+
+
+
     // Use this for initialization
-    void Start () {
+    void Start () 
+    {
+        lightsOn = true;
+        camera.cullingMask |= 1 << LayerMask.NameToLayer("Maze");
+
+        camera.cullingMask &=  ~(1 << LayerMask.NameToLayer("Ghosts"));
+
         body = GetComponent<Rigidbody>();
 	}
 	
+
+
+
 	// Update is called once per frame
-	void Update () {
-        float moveH = Input.GetAxis("Horizontal");
-        float moveV = Input.GetAxis("Vertical");
+	void Update () 
+    {
+        if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D))
+        {
+            body.AddForce (Vector3.right * speed);
+        }
 
-        Vector3 movement = new Vector3(moveH, 0, moveV);
+        if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A))
+        {
+            body.AddForce (-Vector3.right * speed);
+        }
 
-        body.MovePosition(body.position + (movement * speed));
+        if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W))
+        {
+            body.AddForce (Vector3.forward * speed);
+        }
+
+        if (Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S))
+        {
+            body.AddForce (-Vector3.forward * speed);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            flipSwitch();   
+        }
 	}
+
+
+
+
+    void flipSwitch()
+    {
+        if (lightsOn)
+        {
+            print("Lights Off!");
+
+            lightsOn = false;
+
+            camera.cullingMask &=  ~(1 << LayerMask.NameToLayer("Maze"));
+
+            camera.cullingMask |= 1 << LayerMask.NameToLayer("Ghosts");
+        }
+        else
+        {
+            print("Lights On!");
+
+            lightsOn = true;
+
+            camera.cullingMask |= 1 << LayerMask.NameToLayer("Maze");
+
+            camera.cullingMask &=  ~(1 << LayerMask.NameToLayer("Ghosts"));
+        }
+    }
+
+
 
 
     void OnTriggerEnter(Collider collider)
     {
-        print("Collision!!!");
+        print("#TRIGGERED!!!");
         print(collider.gameObject.tag);
+
+        if (collider.gameObject.name == "Door")
+        {
+            print("Door Reached! Going To Next Level...");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        if (collider.gameObject.tag == "Ghost")
+        {
+            print("Dead! Restarting Level...");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        print("Collision!!!");
     }
 }
---
+++ Assets/Scripts/PatrolGhost.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolGhost : MonoBehaviour 
+{
+    private Rigidbody body;
+    public Vector3 firstCoordinates;
+    public Vector3 secondCoordinates;
+    public Vector3 thirdCoordinates;
+    public Vector3 fourthCoordinates;
+    private int currentCoordinates;
+
+
+    // Use this for initialization
+    void Start () {
+        body = GetComponent<Rigidbody>();
+        currentCoordinates = 1;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Mathf.Abs(body.position.x - firstCoordinates.x) < .1 && Mathf.Abs(body.position.z - firstCoordinates.z) < .1 && currentCoordinates == 1){
+            currentCoordinates = 2;
+        }
+        else if (Mathf.Abs(body.position.x - secondCoordinates.x) < .1 && Mathf.Abs(body.position.z - secondCoordinates.z) < .1 && currentCoordinates == 2){
+            currentCoordinates = 3;
+        }
+        else if (Mathf.Abs(body.position.x - thirdCoordinates.x) < .1 && Mathf.Abs(body.position.z - thirdCoordinates.z) < .1 && currentCoordinates == 3){
+            currentCoordinates = 4;
+        }
+        else if (Mathf.Abs(body.position.x - fourthCoordinates.x) < .1 && Mathf.Abs(body.position.z - fourthCoordinates.z) < .1 && currentCoordinates == 4){
+            currentCoordinates = 1;
+        }
+
+        if (currentCoordinates == 1){
+            body.position = Vector3.Lerp(body.position, firstCoordinates, .05f);
+        }
+        if (currentCoordinates == 2){
+            body.position = Vector3.Lerp(body.position, secondCoordinates, .05f);
+        }
+        if (currentCoordinates == 3){
+            body.position = Vector3.Lerp(body.position, thirdCoordinates, .05f);
+        }
+        if (currentCoordinates == 4){
+            body.position = Vector3.Lerp(body.position, fourthCoordinates, .05f);
+        }
+    }
+
+}
